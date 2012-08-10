817d34cf6da0319732e62e78183527c3cc751edc
Jacob Carlborg <doob@me.com>
2012-08-10 13:27:28 +0200

    Update dwt.events.ShellListener.

--- dwt/events/ShellListener.d
+++ dwt/events/ShellListener.d
@@ -12,9 +12,11 @@
  *******************************************************************************/
 module dwt.events.ShellListener;
 
+import dwt.dwthelper.utils;
 
-import dwt.internal.SWTEventListener;
+
 import dwt.events.ShellEvent;
+import dwt.internal.SWTEventListener;
 
 /**
  * Classes which implement this interface provide methods
