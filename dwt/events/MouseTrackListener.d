ac59a6e7940e1e7912fcc5d45e35646ca95c40ad
Jacob Carlborg <doob@me.com>
2012-08-10 13:16:53 +0200

    Update dwt.events.MouseTrackListener.

--- dwt/events/MouseTrackListener.d
+++ dwt/events/MouseTrackListener.d
@@ -12,9 +12,11 @@
  *******************************************************************************/
 module dwt.events.MouseTrackListener;
 
+import dwt.dwthelper.utils;
 
-import dwt.internal.SWTEventListener;
+
 import dwt.events.MouseEvent;
+import dwt.internal.SWTEventListener;
 
 /**
  * Classes which implement this interface provide methods
