4d3cc811226e8fb1c28fa6de5c8716356318a5ef
Jacob Carlborg <doob@me.com>
2012-08-10 12:37:34 +0200

    Update dwt.events.ExpandAdapter.

--- dwt/events/ExpandAdapter.d
+++ dwt/events/ExpandAdapter.d
@@ -12,6 +12,9 @@
  *******************************************************************************/
 module dwt.events.ExpandAdapter;
 
+import dwt.dwthelper.utils;
+
+
 import dwt.events.ExpandListener;
 
 /**
