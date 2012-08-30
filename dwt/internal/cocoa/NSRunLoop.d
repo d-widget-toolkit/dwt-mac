12990d06c8645984ef0ed3483917258e85e38e9f
Jacob Carlborg <doob@me.com>
2012-08-30 21:28:04 +0200

    Update dwt.internal.cocoa.NSRunLoop.

--- dwt/internal/cocoa/NSRunLoop.d
+++ dwt/internal/cocoa/NSRunLoop.d
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -34,10 +34,6 @@ public this(objc.id id) {
 
 public this(cocoa.id id) {
     super(id);
-}
-
-public void addTimer(NSTimer timer, NSString mode) {
-    OS.objc_msgSend(this.id, OS.sel_addTimer_forMode_, timer !is null ? timer.id : 0, mode !is null ? mode.id : 0);
 }
 
 public void addTimer(NSTimer timer, NSString mode) {
